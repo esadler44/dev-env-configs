047fcc9ea3e7e1d5ae32fad827b7f62afe3ebb20
elliot-wisdom <elliot@datawisdom.ai>
2024-07-28 20:03:57 -0700

    update script for fish and iterm theme

--- install.fish
+++ install.fish
@@ -7,15 +7,22 @@ echo "Restarting window management..."
 yabai --restart-service
 skhd -r
 
-echo "Fetching themes..."
+echo "Fetching Tokyonight theme..."
 mkdir downloads
 git clone https://github.com/folke/tokyonight.nvim.git downloads
-
+cp downloads/extras/fish_themes/tokyonight_night.theme ~/.config/fish/themes/Tokyonight_Night.theme
+cp downloads/extras/iterm/tokyonight_night.itermcolors ./Tokyonight\ Night.itermcolors
 rm -rf downloads
 
-echo "Applying themes..."
-fish_config theme choose Tokyonight
+echo "Applying Tokyonight theme..."
+fish_config theme choose Tokyonight_Night
+yes | fish_config theme save
 
-echo "Make sure to install the following:"
-echo "Set the iTerm2 color scheme to Tokyonight"
-
+echo "Make sure to setup the folloing manually:"
+echo "Set the iTerm2 color scheme to Tokyonight Night as well"
+echo "Go to Profiles > Colors > Color Presets > Import... and select the Tokyonight Night.itermcolors file"
+echo "Then set the preset to Tokyonight Night"
+echo "Go to the Window tab and set the Transparency to 40%"
+echo "Keep background colors opaque"
+echo "Blur on, Radius 25"
+echo "Style: No Title Bar"
